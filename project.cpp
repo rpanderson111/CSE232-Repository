f888ead17989bd04b0c758616d1c638e335d15c6
Ryan <rpanderson111@gmail.com>
2019-11-12 21:10:59 +0000

    Changed the project.cpp

--- project.cpp
+++ project.cpp
@@ -4,38 +4,32 @@ using std::cout; using std::endl; using std::cin;
 using std::vector;
 #include<string>
 using std::string;
-#include<functional>
-using std::multiplies; using std::minus; using std::plus;
-#include<numeric>
-using std::accumulate;
-#include<algorithm>
-using std::remove_if; using std::copy; using std::count_if; using std::sort; using std::copy_if;
-#include<iterator>
-using std::ostream_iterator; using std::back_inserter;
 #include<fstream>
 using std::ifstream;
-#include<typeinfo>
-#include<iomanip>
-using std::setprecision; using std::fixed;
 
 int main() {
+    //pulls in file
     ifstream input ("common_passwords.txt");
+    
+    //initializes vector for in common passwords, the string for user's input, and the string for theword within the file
     vector<string> in_common;
-    string comparing_word;
-    string user_input;
+    string comparing_word, user_input;
     cout << "Give me a password: ";
     cin >> user_input;
     int input_size = user_input.size();
     cout << endl << "You provided a password of " << user_input << endl;
     int final_diff = 100000;
+    //loop through each word within file
     while (true) {
         if (input >> comparing_word) {
+            //compares user's password to word within file, giving how many letters shared there are (shared)
             int shared = 0;
             for (int i = 0; i < input_size; i++) {
                 if (user_input[i] == comparing_word[i]) {
                     shared += 1;
                 }
             }
+            //checks length of user's word and word within file, then finds the difference
             int comparing_size = comparing_word.size();
             int diff;
             if (input_size >= comparing_size) {
@@ -43,7 +37,7 @@ int main() {
             } else {
                 diff = comparing_size - shared;
             }
-            //cout << "Shared: " << shared << " Size: " << comparing_size << " Difference: " << diff << endl;
+            //checks length of in_common vector, then appends word if meets criteria
             if (in_common.empty() || diff == final_diff) {
                 in_common.push_back(comparing_word);
             }
@@ -54,11 +48,13 @@ int main() {
             }
         } else if (input.eof()) {
             break;
+        //moves to next word in file
         } else {
             input.clear();
             input.ignore();
         }
     }
+    //final output
     cout << "The most similar passwords to " << user_input << " are: " << endl;
     sort(in_common.begin(), in_common.end());
     for (auto i: in_common) {
